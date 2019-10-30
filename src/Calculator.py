ad2784a8e73dcc8f98d205f5d5527dc8918f2073
dtchin2 <dtchin2@gmail.com>
2019-10-29 21:50:52 -0400

    Fix: renamed methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,32 +3,32 @@ import math
 # create method to add, subtract, multiply, divide, square and square root
 
 # add (return the value of the sum of num1 and num2)
-def add(num1, num2):
+def add_vals(num1, num2):
     return num1 + num2
 
 
 # subtract (return the value of the difference of num1 and num2)
-def subtract(num1, num2):
+def subtract_vals(num1, num2):
     return num1 - num2
 
 
 # multiply(return the value of the product of num1 and num2)
-def multiply(num1, num2):
+def multiply_vals(num1, num2):
     return num1 * num2
 
 
-def divide(num1, num2):
+def divide_vals(num1, num2):
     float(num1)
     float(num2)
 
     return num1 / num2
 
 
-def square_root(num):
+def square_root_val(num):
     return math.sqrt(num)
 
 
-def square_number(num):
+def square_number_val(num):
     return num * num
 
 

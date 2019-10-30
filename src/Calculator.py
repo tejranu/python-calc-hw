617c754a40421576730a15f4ad7a6941e76bf8ac
dtchin2 <dtchin2@gmail.com>
2019-10-29 21:39:30 -0400

    Feature: Created add and subtract methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,4 +2,16 @@
 
 # create method to add, subtract, multiply, divide, square and square root
 
-# add (return the value of the addition of num1 and num2)
+# add (return the value of the sum of num1 and num2)
+def add(num1, num2):
+    return num1 + num2
+
+
+# subtract (return the value of the difference of num1 and num2)
+def subtract(num1, num2):
+    return num1 - num2
+
+
+
+
+

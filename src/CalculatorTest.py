740f275251928654277e1e71ffc89105c5184be3
dtchin2 <dtchin2@gmail.com>
2019-10-31 19:25:53 -0400

    Feature: Successful Addition method test

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -4,7 +4,7 @@ from Calculator import Calculator
 from CSVFileReader import CSVFileReader
 
 
-class MyTestCase(unittest.TestCase):
+class SubtractCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
 
@@ -13,11 +13,25 @@ class MyTestCase(unittest.TestCase):
 
     def test_subtraction(self):
         self.calculator = Calculator()
-        test_sub_data = CSVFileReader('/src/subtraction.csv').file_data
-        for row in test_sub_data:
+        test_data = CSVFileReader('/src/subtraction.csv').file_data
+        for row in test_data:
             self.assertEqual(self.calculator.subtract(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.value, int(row['Result']))
+        test_data = []
 
+
+class AddCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_calculator_instantiation(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def add_method_test(self):
+        add_data = CSVFileReader('/src/addition.csv').file_data
+        for row in add_data:
+            self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.value, int(row['Result']))
 
 
 

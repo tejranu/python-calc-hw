cd1d3a957a1aa0a6d072923566d9e227b3d9e722
dtchin2 <dtchin2@gmail.com>
2019-10-30 20:30:39 -0400

    Task: Added csv files

--- src/CSVFileReader.py
+++ src/CSVFileReader.py
@@ -1,3 +1,10 @@
 import csv
 from pprint import pprint
 
+class CSVFileReader:
+    file_data = []
+
+    def __init__(self):
+        # when constructor is created
+        # open the file
+        pass

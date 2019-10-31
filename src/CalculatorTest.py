2a7c6ce61faa71964c24861dd0539e36985536e7
dtchin2 <dtchin2@gmail.com>
2019-10-31 19:40:48 -0400

    Feature: Successful Multiply and Divide Test runs

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -4,6 +4,14 @@ from Calculator import Calculator
 from CSVFileReader import CSVFileReader
 
 
+class CalculatorInstantiationCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+        
+    def test_calculator_instantiation(self):
+        self.assertIsInstance(self.calculator, Calculator)
+        
+
 class SubtractCase(unittest.TestCase):
     def setUp(self) -> None:
         self.calculator = Calculator()
@@ -33,6 +41,30 @@ class AddCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.value, int(row['Result']))
 
+
+class MultiplyCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_calculator_instantiation(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def multiply_method_test(self):
+        mult_data = CSVFileReader('/src/multiply.csv').file_data
+        for row in mult_data:
+            self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.value, int(row['Result']))
+
+
+class DivideCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def divide_method_test(self):
+        div_data = CSVFileReader('/src/division.csv').file_data
+        for row in div_data:
+            self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.value, int(row['Result']))
 
 
 

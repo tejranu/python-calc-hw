fe2588aca2168315657229d5b8859acf25ab933f
dtchin2 <dtchin2@gmail.com>
2019-11-03 21:18:36 -0500

    Fix: Changed assert statments in the squarert_test.py test

--- src/squarert_test.py
+++ src/squarert_test.py
@@ -11,8 +11,8 @@ class MyTestCase(unittest.TestCase):
     def test_square_rt_method_test(self):
         sqrt_data = CSVFileReader('/src/squareroot.csv').file_data
         for row in sqrt_data:
-            self.assertEqual(self.calculator.square_root(float((row['Value 1']))), float((row['Result'])))
-            self.assertEqual(self.calculator.value, float((row['Result'])))
+            self.assertAlmostEqual(self.calculator.square_root(float((row['Value 1']))), float((row['Result'])))
+            self.assertAlmostEqual(self.calculator.value, float((row['Result'])))
 
 
 if __name__ == '__main__':

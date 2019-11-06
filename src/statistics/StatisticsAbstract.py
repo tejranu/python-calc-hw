135d20864c9f1cb2d84d61bab0caa0b65c1e3f12
dtchin2 <dtchin2@gmail.com>
2019-11-05 19:37:40 -0500

    Feature: Added methods to the abstract StatisticsAbstract class

--- src/statistics/StatisticsAbstract.py
+++ src/statistics/StatisticsAbstract.py
@@ -7,5 +7,67 @@ class StatisticsAbstract(ABC):
     def __init__(self):
         pass
 
+    @abstractmethod
+    def population_mean(self):
+        pass
+
+    @abstractmethod
+    def median(self):
+        pass
+
+    @abstractmethod
+    def mode(self):
+        pass
+
+    @abstractmethod
+    def pop_standard_deviation(self):
+        pass
+
+    @abstractmethod
+    def variance_pop_proportion(self):
+        pass
+
+    @abstractmethod
+    def zscore(self):
+        pass
+
+    @abstractmethod
+    def standardized_score(self):
+        pass
+
+    @abstractmethod
+    def population_corre_coefficient(self):
+        pass
+
+    @abstractmethod
+    def confidence_interval(self):
+        pass
+
+    @abstractmethod
+    def population_variance(self):
+        pass
+
+    @abstractmethod
+    def p_value(self):
+        pass
+
+    @abstractmethod
+    def proportion(self):
+        pass
+
+    @abstractmethod
+    def sample_mean(self):
+        pass
+
+    @abstractmethod
+    def sample_standard_deviation(self):
+        pass
+
+    @abstractmethod
+    def variance_sample_proportion(self):
+        pass
+
     
+
+
 

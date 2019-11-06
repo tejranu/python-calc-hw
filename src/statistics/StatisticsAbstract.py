d2e536a54bff1f033f592b7478c555414b305a1b
dtchin2 <dtchin2@gmail.com>
2019-11-05 19:29:42 -0500

    Task: Added statistics, csv folder and abstract class

---
+++ src/statistics/StatisticsAbstract.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+# abstract class to contain all methods to be implemented
+
+
+class StatisticsAbstract(ABC):
+
+    def __init__(self):
+        pass
+
+    
+

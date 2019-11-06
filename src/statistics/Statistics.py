74b5ff9c4d8643611e18f2c43f9ce318026b7cd7
dtchin2 <dtchin2@gmail.com>
2019-11-05 23:59:00 -0500

    Fix: Method made non static method reference to population_mean and medin methods

--- src/statistics/Statistics.py
+++ src/statistics/Statistics.py
@@ -1,44 +1,57 @@
 from src.statistics.StatisticsAbstract import StatisticsAbstract
 
 
+def population_mean(number_list):
+    # population mean formula:
+    number_list = list(number_list)
+    sum_value = 0
+
+    for x in number_list:
+        sum_value = sum(number_list)
+
+    # take the sum of the items and then divide it by the number of items
+    number_count = len(number_list)
+
+    result = sum_value / number_count
+    # then return that value
+    return result
+
+
+def median(number_list):
+    # first sort the list
+    number_list = list(number_list)
+    number_list.sort()
+
+    # take the list and get the value at the middle of the number list
+    if len(number_list) % 2 is not 0:
+        middle_val_index = int(len(number_list) / 2)
+
+        middle_val = number_list[middle_val_index]
+
+        # self.result = middle_val
+        return middle_val
+
+    else:
+        # if the number count of the list is even, take the middle two numbers... add them and divide by 2
+        middle_val_index = int((len(number_list) - 1) / 2)
+        after_middle_index = middle_val_index + 1
+        # self.result = int((float(number_list[middle_val_index]) + float(number_list[after_middle_index])) / 2.0)
+        return (float(number_list[middle_val_index]) + float(number_list[after_middle_index])) / 2
+
+
+#        return self.result
+
+
 class Statistics(StatisticsAbstract):
 
     result = 0
 
     def population_mean(self, number_list):
-        # population mean formula:
-        number_list = list(number_list)
-        sum_value = 0
-
-        for x in number_list:
-            sum_value = sum(number_list)
-
-        # take the sum of the items and then divide it by the number of items
-        number_count = len(number_list)
-
-        self.result = sum_value / number_count
-        # then return that value
+        self.result = population_mean(number_list)
         return self.result
 
     def median(self, number_list):
-        # first sort the list
-        number_list = list(number_list)
-        number_list.sort()
-
-        # take the list and get the value at the middle of the number list
-        if len(number_list) % 2 is not 0:
-            middle_val_index = int(len(number_list) / 2)
-
-            middle_val = number_list[middle_val_index]
-
-            self.result = middle_val
-        else:
-            # if the number count of the list is even, take the middle two numbers... add them and divide by 2
-            middle_val_index = int(len(number_list) / 2)
-            after_middle_index = middle_val_index + 1
-
-            self.result = (float(number_list[middle_val_index]) + float(number_list[after_middle_index])) / 2
-
+        self.result = median(number_list)
         return self.result
 
     def mode(self):
